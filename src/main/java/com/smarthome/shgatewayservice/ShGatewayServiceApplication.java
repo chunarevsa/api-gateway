d16d4958cdf618a4eb208aea464eb364aa51dadc
Chunarev_Sergey <sergey.chunarev@ural.rt.ru>
2023-06-29 13:50:12 +0200

    init

---
+++ src/main/java/com/smarthome/shgatewayservice/ShGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smarthome.shgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShGatewayServiceApplication.class, args);
+    }
+
+}
