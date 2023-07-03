8ffcf6d19a1f10d0ba02b394862b1917ea103df3
Chunarev_Sergey <sergey.chunarev@ural.rt.ru>
2023-07-03 21:43:28 +0200

    clearing

--- src/main/java/com/smarthome/shgatewayservice/ShGatewayServiceApplication.java
+++ src/main/java/com/smarthome/shgatewayservice/ShGatewayServiceApplication.java
@@ -2,6 +2,7 @@ package com.smarthome.shgatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class ShGatewayServiceApplication {
