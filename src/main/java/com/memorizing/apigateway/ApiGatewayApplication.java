3791c92fa1725198ead450af80d04c8a36554e25
Chunarev Sergei <chunarev.se.a@gmail.com>
2024-03-02 20:53:42 +0100

    update build.gradle

--- src/main/java/com/memorizing/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/memorizing/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.apigateway;
+package com.memorizing.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
